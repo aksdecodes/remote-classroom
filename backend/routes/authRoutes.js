5f119669c8309512909f1c557d4fff8fabb0dfd1
Akshay <akshayofficial212@gmail.com>
2026-04-21 09:40:43 +0530

    fix route test

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,6 +6,9 @@ const { protect } = require("../middleware/authMiddleware");
 
 // Routes
 router.post("/register", register);
+router.get('/register', (req, res) => {
+  res.send("Register route working");
+});
 router.post("/login", login);
 router.get("/me", protect, getMe);
 
