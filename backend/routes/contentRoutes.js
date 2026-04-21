82021cfddb17f544e19c696ca8ea264d56d4d387
Akshay <akshayofficial212@gmail.com>
2026-04-21 08:19:07 +0530

    fix content model case issue

--- backend/routes/contentRoutes.js
+++ backend/routes/contentRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Content = require("../models/Content");
+const Content = require("../models/content");
 
 // GET all content
 router.get("/", async (req, res) => {
